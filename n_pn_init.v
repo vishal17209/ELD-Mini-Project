490a31c10be512a9d842b3c1296d2266d7e9485f
vishal2314 <32518867+vishal2314@users.noreply.github.com>
2018-11-20 00:21:46 +0530

    Comments added to top_init.v and n_pn_init.v

--- n_pn_init.v
+++ n_pn_init.v
@@ -1,25 +1,28 @@
 `timescale 1ns / 1ps
 
+/*
+    Module to generate the PN sequence from the initial value entered from the user
+*/
 module n_pn_init(next,reset,seq,N, char_poly, init);
-    input [7:0] init;
-    input next,reset;
-    input [3:0] N;
-    input [12:0] char_poly;
-    output reg [12:0] seq = 12'd0;
+    input [7:0] init;       //initial value of the sequence
+    input next,reset;       //next value and reset signal
+    input [3:0] N;          //size of the current characterstic polynomial
+    input [12:0] char_poly; //charcterstic polynomial
+    output reg [12:0] seq = 12'd0;  //generated sequence
     integer i;
     always@(posedge next or posedge reset)
     begin
         if (reset)
-            seq <= init;
+            seq <= init;        //reseting the sequence to the init value. The sequence should be reset to change the initial value of the sequence.
         else
         begin
-            for (i =0;i<13;i= i+1)
+            for (i =0;i<13;i= i+1)  //iterated over the sequence's current value
             begin
-               if (i<N-1)
-               begin
-                    seq[i] <= seq[i+1];
+               if (i<N-1)           //if iterator is less than N-1 left shift the sequence
+               begin    
+                    seq[i] <= seq[i+1];   
                end
-               else if (i == N-1)
+               else if (i == N-1)       //for i equal to N-1 calculate the bitwise AND with char_poly to get the bits which are 1 and then take their XOR
                     seq[N-1] <= ^(seq & char_poly);
                else
                     seq[i] <= 0;
--- top_init.v
+++ top_init.v
@@ -1,33 +1,45 @@
 `timescale 1ns / 1ps
 
+/*
+    top module for PN sequence generator which generated the masked value 
+    according to the output mask entered
+*/
 module top_init(clk,next, rst, cathode,anode, num, init,mask);
-    input [5:0] init;
-    input clk,next,rst;
-    input [3:0] num;
-    output [7:0] cathode;
-    output [3:0] anode;
-    input [5:0] mask;
+    input [5:0] init;           //inital value for the sequence
+    input clk,next,rst;         //clock, next value and reset signal respectively
+    input [3:0] num;            //selection line for the characterstic polynomial for maximum sequence length
+    output [7:0] cathode;       //cathode for 7 seven segment display
+    output [3:0] anode;         //anode for 7 segment display
+    input [5:0] mask;           //output mask polynomial
     
-    wire [12:0] seq_out;
-    wire [3:0] one,ten,hundred,thousand;
-    wire ssd_clk,deb_clk,deb_rst,deb_nxt;
-    wire [15:0] char_poly;
-    wire [12:0] masked_out;
-    freq_divide #(17) E(clk,ssd_clk);
+    //Variables
+    wire [12:0] seq_out;                    //sequence output from n_pn_init
+    wire [3:0] one,ten,hundred,thousand;    //BCD outputs
+    wire ssd_clk,deb_clk,deb_rst,deb_nxt;   //debounced signals and clocks
+    wire [15:0] char_poly;                  //characterstic polynomial
+    wire [12:0] masked_out;                 //masked output
+
+    //frequency dividers
+    freq_divide #(17) E(clk,ssd_clk);       
     freq_divide #(19) F(clk,deb_clk);
     
+    //creating a pulse for reset and next
     deb_pulse G(deb_clk,rst,deb_rst);
     deb_pulse H(deb_clk,next,deb_nxt);
     
     //example_8_bit #(.N(4),.INITIAL(5)) I(deb_nxt,deb_rst,seq_out, char_polynomial);
     
+    //getting the characterstic polynomial from BRAM
     blk_mem_gen_0 C(clk,1,num,char_poly);
     
+    //generating a new value in the sequence
     n_pn_init D(deb_nxt, deb_rst, seq_out, num, char_poly[12:0], init);
     
     //bcd_bin_8_bit A({4'b0,seq_out},thousand,hundred,ten,one);
+    //get the masked value for the enter masked output
     assign masked_out = (mask==5'b0)?seq_out:^(seq_out&mask);
     
+    //convert the number to BCD and display it on the seven segment display
     bcd_n_bit #(13) A(masked_out,hundred,ten,one,thousand);
     seven_seg_onboard B(one,ten,hundred,thousand,cathode,anode,ssd_clk);
     
